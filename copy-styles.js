d2463f2e3ca63097db6f43273785cb0b086769f5
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-03-15 21:50:39 +0100

    Simplify building styles process

--- copy-styles.js
+++ copy-styles.js
@@ -7,11 +7,3 @@ fs.copyFile('src/Clock.less', 'dist/Clock.less', (error) => {
   // eslint-disable-next-line no-console
   console.log('Clock.less copied successfully.');
 });
-
-fs.copyFile('src/Clock.css', 'dist/Clock.css', (error) => {
-  if (error) {
-    throw error;
-  }
-  // eslint-disable-next-line no-console
-  console.log('Clock.css copied successfully.');
-});
