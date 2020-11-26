ebe24466fc22d2983f1c745c243b48ee2cfcb18d
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-11-26 10:45:05 +0100

    Update sample

---
+++ sample/Sample.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { useSetInterval } from '@wojtekmaj/react-hooks';
+import Clock from 'react-clock';
+
+import './Sample.less';
+
+export default function Sample() {
+  const [value, onChange] = useState(new Date());
+
+  useSetInterval(() => {
+    const now = new Date();
+    onChange(now);
+  }, 1000);
+
+  return (
+    <div className="Sample">
+      <header>
+        <h1>react-clock sample page</h1>
+      </header>
+      <div className="Sample__container">
+        <main className="Sample__container__content">
+          <Clock
+            size={200}
+            value={value}
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
