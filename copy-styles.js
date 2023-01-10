321f8d8cff10a352e7ab5eec6e6b64d1636a41b8
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-01-10 14:21:11 +0100

    Replace Less with CSS

--- copy-styles.js
+++ copy-styles.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-fs.copyFile('src/Clock.less', 'dist/Clock.less', (error) => {
+fs.copyFile('src/Clock.css', 'dist/Clock.css', (error) => {
   if (error) {
     throw error;
   }
   // eslint-disable-next-line no-console
-  console.log('Clock.less copied successfully.');
+  console.log('Clock.css copied successfully.');
 });
