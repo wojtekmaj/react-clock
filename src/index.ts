95b22d23d1a1ee65c4bdc6a6ddf5c2cd554c957a
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-06-05 22:39:58 +0200

    Simplify component props type export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import Clock from './Clock';
 
-import type { ClockProps } from './Clock';
+export type { ClockProps } from './Clock';
 
 export { Clock };
-
-export type { ClockProps };
 
 export default Clock;
