2a3a46045d7d6d3eae219f7880271ca78afe05bb
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-07-26 00:44:11 +0200

    Simplify types
    Closes #143

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,18 +1,10 @@
-type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
-  ? Acc[number]
-  : Enumerate<N, [...Acc, Acc['length']]>;
-
-type Range<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>> | T;
-
 export type ClassName = string | null | undefined | (string | null | undefined)[];
 
-export type GreaterThanOrEqualTo<F extends number> = Range<F, 999>;
-
-export type HandLength = Range<0, 100>;
+export type HandLength = number; // Range<0, 100>;
 
 export type OppositeHandLength = number; // Range<-100, 100>;
 
-export type HandWidth = GreaterThanOrEqualTo<0>;
+export type HandWidth = number; // GreaterThanOrEqualTo<0>;
 
 export type MarkLength = HandLength;
 
