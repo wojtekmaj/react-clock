1acb05d800025dde13fa0d2b012d429801c1d7ae
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-07-27 12:30:12 +0200

    Replace custom script in copy-styles script with cpy

--- copy-styles.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs';
-
-fs.copyFile('src/Clock.css', 'dist/Clock.css', (error) => {
-  if (error) {
-    throw error;
-  }
-  // eslint-disable-next-line no-console
-  console.log('Clock.css copied successfully.');
-});
