7e436f7dcdddb861c51a55e2086c8d930d90a60e
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-09-30 20:56:34 +0200

    Use new JSX transform in all sample and test suites

--- test/Test.tsx
+++ test/Test.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Clock from 'react-clock';
 import 'react-clock/dist/Clock.css';
 import { useSetInterval } from '@wojtekmaj/react-hooks';
